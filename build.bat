4bc93f640e05806c3c3be3deaad1afb349890990
Duke Jodi <54733921+jodi-one@users.noreply.github.com>
2020-02-14 11:37:01 +0100

    Added linux build file and .gitignore for gradle.properties cause that is where proxies might be configred.

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 gradle clean
 groovy copyJars.groovy
 gradle shadowJar
-mvn install:install-file "-Dfile=build/libs/jodi_odi-12.2.1.3.2.4.jar" "-DgroupId=one.jodi" "-DartifactId=jodi_odi" "-Dversion=12.2.1.3.2.4" "-Dpackaging=jar"
+mvn install:install-file "-Dfile=build/libs/jodi_odi-12.2.1.3.2.6.jar" "-DgroupId=one.jodi" "-DartifactId=jodi_odi" "-Dversion=12.2.1.3.2.6" "-Dpackaging=jar"
