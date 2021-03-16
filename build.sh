2912e2170e19e64c54aec9fe17fb8360269b55ca
Duke Jodi <54733921+jodi-one@users.noreply.github.com>
2021-03-16 09:28:47 +0100

    ODI-12.2.1.4.0 sdk

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ if [ $status -gt 0 ]
   echo "shadowJar failed";
   exit 1;
 fi
-mvn install:install-file -Dfile=./build/libs/jodi_odi-12.2.1.3.2.6.jar -DgroupId=one.jodi -DartifactId=jodi_odi -Dversion=12.2.1.3.2.6 -Dpackaging=jar
+mvn install:install-file -Dfile=./build/libs/jodi_odi-12.2.1.4.1.0.jar -DgroupId=one.jodi -DartifactId=jodi_odi -Dversion=12.2.1.4.1.0 -Dpackaging=jar
 status=$?
 if [ $status -gt 0 ]
 then
--- copyJars.groovy
+++ copyJars.groovy
@@ -2,7 +2,7 @@ import static groovy.io.FileType.*
 import java.nio.file.*
 
 // def odiSrcDir = new File('/u01/app/odi')
-def odiSrcDir = new File('/u01/app/oracle/product/12.2.1.4.0/odihome_1/')
+def odiSrcDir = new File('D:/products/12.2.1.4.0/odihome_1/')
 def libFolder = 'src/main/resources/lib'
 def trgtDir = new File(libFolder)
 def odiJarNames = [
@@ -64,7 +64,7 @@ def odiJarNames = [
         'org.codehaus.jackson.jackson-core-asl.jar',
         'org.glassfish.javax.json.jar',
         'javax.management.j2ee.jar',
-     //   'oracle.ucp.jar', // this should come from patch 26261906 otherwise we get connection closed.
+        //   'oracle.ucp.jar', // this should come from patch 26261906 otherwise we get connection closed.
         'javax.persistence.jar',
         'dms.jar',
         'ojdl.jar',
@@ -122,6 +122,7 @@ def odiJarNames = [
         'oracle.odi.tp.clientLib.jar',
         'oraclepki.jar'
 ]
+
 
 def copyFiles = {
     if( ((File) it).getAbsolutePath().contains(".patch_storage")){return}
