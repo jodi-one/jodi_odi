aa7b553141aa3512a9ae072b588c127390adb5dc
Eelse Pieters <eelse@jodi.one>
2021-04-22 11:19:34 +0200

    Removed the windows build file, just use the bash one in Git bash on windows

--- build.bat
+++
@@ -1,5 +0,0 @@
-@SET odiVersion=12.2.1.4.0.1
-gradle clean
-groovy copyJars.groovy
-gradle shadowJar
-mvn install:install-file "-Dfile=build/libs/jodi_odi-%odiVersion%.jar" "-DgroupId=one.jodi" "-DartifactId=jodi_odi" "-Dversion=%odiVersion%" "-Dpackaging=jar"
